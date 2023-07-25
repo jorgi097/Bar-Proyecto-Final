388ef72c95ee84bfa4e9a8fb47582e9f83101355
jorge <77767513+jorgi097@users.noreply.github.com>
2023-07-25 06:37:57 -0600

    VARIOS CAMBIOS

--- main.c
+++ main.c
@@ -23,17 +23,17 @@ void clean();
 
 //Enum
 enum turno{
-    Manana = 1,
-    Tarde = 2,
-    Noche = 3
+    Matutino = 1,
+    Vespertino = 2,
+    Nocturno = 3
 };
 
 
 enum puesto{
-    Mesero,
-    Cocinero,
-    Gerente,
-    Bartender
+    Mesero = 1,
+    Cocinero = 2,
+    Gerente = 3,
+    Bartender = 4
 };
 
 
@@ -70,10 +70,10 @@ int mainOpc = 500;
 mesa mesas[15]; //Array con las mesas del bar
 
 empleado empleados[20] = { //Array con todos los empleados
-    {false, 0, "Jon Doe", 0, Noche, Mesero},
-    {true, 1, "Juan", 25, Noche, Cocinero},
-    {true, 2, "Martha", 23, Noche, Mesero},
-    {true, 3, "Yolanda", 21, Noche, Mesero},
+    {false, 0, " ", 0, 0, 0},
+    {true, 1, "Juan", 25, 3, Mesero},
+    {true, 2, "Marta", 23, 3, Mesero},
+    {true, 3, "Yolanda", 21, 3, Mesero},
 };
 
 int numEmpleadosUsados = 0;
@@ -185,14 +185,15 @@ void adminEmpleados(){ // Menu administración de empleados
     int adminEmpleadosOpc;
 
     while(adminEmpleadosOpc != 0){
+        numEmpleadosUsados = 0;
         numEmpleadosRegistrados();
-
+        system("cls");
         printf("Hay %d empleados dados de alta en este momento\n\n", numEmpleadosUsados);
-
-        printf(" Numero de Empleado |         Nombre |     edad |     turno |     puesto |\n");
+        printf("TURNO: Matutino = 1 | Vespertino = 2 | Nocturno = 3\n\nPUESTO: Mesero = 1 | Cocinero = 2 | Gerente = 3 | Bartender = 4\n\n");
+        printf(" NUMERO DE EMPLEADO |         NOMBRE |     EDAD |     TURNO |     PUESTO |\n"); // FALTA QUE IMPRIMA EL TURNO Y EL PUESTO
         for(unsigned int i = 1; i < sizeof(empleados) / sizeof(empleados[0]); i++){
             if(empleados[i].alta){
-                printf("%19d | %14s |%9d |\n", empleados[i].numEmpleado, empleados[i].nombre, empleados[i].edad);
+                printf("%19d | %14s |%9d |%10d |%11d |\n", empleados[i].numEmpleado, empleados[i].nombre, empleados[i].edad, empleados[i].turno, empleados[i].puesto);
             }
         }
 
@@ -201,6 +202,7 @@ void adminEmpleados(){ // Menu administración de empleados
         printf("1) Alta empleado\n");
         printf("2) Modificar empleado\n");
         printf("3) Dar de baja empleado\n");
+        printf("0) Regresar\n");
 
         scanf("%d", &adminEmpleadosOpc);
 
@@ -221,18 +223,68 @@ void adminEmpleados(){ // Menu administración de empleados
 
 //Funciones secundarias
 
-void altaEmpleado(){
-    int empleadoOpc;
-    printf("Ingresa un numero de empleado a dar de alta: ");
-    scanf("%d", &empleadoOpc);
+void altaEmpleado(){ // AGREGAR DAR DE ALTA EMPLEADOS PREVIAMENTE DADOS DE BAJA
+    printf("Ingresa el nombre del empleado a dar de alta: ");
+    scanf("%s", empleados[numEmpleadosUsados + 1].nombre);
+    printf("Ingresa la edad del empleado a dar de alta: ");
+    scanf("%d", &empleados[numEmpleadosUsados + 1].edad);
+    printf("Ingresa el turno del empleado a dar de alta (1: Matutino, 2: Vespertino, 3: Nocturno): ");
+    scanf("%d", (int*)&empleados[numEmpleadosUsados + 1].turno);
+    printf("Ingresa el puesto del empleado a dar de alta (1: Mesero, 2: Cocinero, 3: Gerente, 4: Bartender): ");
+    scanf("%d", (int*)&empleados[numEmpleadosUsados + 1].puesto);
+    empleados[numEmpleadosUsados + 1].alta = true;
+    empleados[numEmpleadosUsados + 1].numEmpleado = numEmpleadosUsados + 1;
 }
 
 void modificacionEmpleado(){
+    int numeroEmpleadoOpc, modificarOpc;
+    system("cls");
+    printf("Ingresa el numero de empleado del empleado a modificar: \n");
+    scanf("%d", &numeroEmpleadoOpc);
+    while(modificarOpc != 0){
+        printf("Que deseas modificar de %s?\n", empleados[numeroEmpleadoOpc].nombre);
+        printf("1) Nombre\n");
+        printf("2) Edad\n");
+        printf("3) Turno\n");
+        printf("4) Puesto\n");
+        printf("0) Regresar\n");
 
+        scanf("%d", &modificarOpc);
+
+        switch(modificarOpc){
+        case 1:
+            printf("\nIngresa el nombre del empleado: \n");
+            scanf("%s", empleados[numeroEmpleadoOpc].nombre);
+            printf("Cambio ralizado\n\n");
+            break;
+        case 2:
+            printf("\nIngresa la edad del empleado: ");
+            scanf("%d", &empleados[numeroEmpleadoOpc].edad);
+            printf("Cambio ralizado\n\n");
+            break;
+        case 3:
+            printf("\nIngresa el turno del empleado (1: Matutino, 2: Vespertino, 3: Nocturno): \n");
+            scanf("%d", (int*)&empleados[numeroEmpleadoOpc].turno);
+            printf("Cambio ralizado\n\n");
+            break;
+        case 4:
+            printf("\nIngresa el puesto del empleado(1: Mesero, 2: Cocinero, 3: Gerente, 4: Bartender): \n");
+            scanf("%d", (int*)&empleados[numeroEmpleadoOpc].puesto);
+            printf("Cambio ralizado\n\n");
+            break;
+
+        default:
+            break;
+        }
+    }
 }
 
 void bajaEmpleado(){
-
+    int numeroEmpleadoOpc;
+    system("cls");
+    printf("Ingresa el numero de empleado a dar de baja: \n");
+    scanf("%d", &numeroEmpleadoOpc);
+    empleados[numeroEmpleadoOpc].alta = false;
 }
 
 
@@ -252,10 +304,9 @@ bool buscar(int numEmpleado){ // Busca si un empleado esta dado de alta segun su
     for(int i = 1; i < numEmpleados; i++){
         if(empleados[i].numEmpleado == numEmpleado && empleados[i].alta){
             return true;
-        } else{
-            return false;
         }
     }
+    return false;
 }
 
 
